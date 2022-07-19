877d2d2e9170e3f033fbcfdab7677e126c18f5d0
EmiMolina248 <91500085+EmiMolina248@users.noreply.github.com>
2022-07-19 17:07:29 -0300

    casos y tipos de umbral(es)

--- emi_jere_ema.ino
+++ emi_jere_ema.ino
@@ -10,37 +10,69 @@
 //#include <Servo.h>
 
 //Servo myservo;  // create servo object to control a servo
-int sensorValue, servoValue,cont;
-int arraySensor[10];
+int sensorValue,regulaUmbral, servoValue,cont;
+int optionUmbral = 2;
+int led;
+const int largoArray = 100; // variable que contiene el largo del array del sensor
+int arraySensor[largoArray]; 
+//int arrayRegulador[largoArray];
+
+
 
 int promValues(int b){
   int sum  = 0;
-  for(int i= 1;i<10;i++){
-   arraySensor[i-1] = arraySensor[i]; 
+  for(int i= 1;i<largoArray ;i++){
+   arraySensor[i-1] = arraySensor[i]; //funcion para sacar el promedio de los valores del sensor guardados en el array
    sum += arraySensor[i-1];
   }
   arraySensor[9] = b;
   sum += b ; 
-  return sum/10;
+  return sum/largoArray ;
 }
+
+
+
+int getUmbral(int p,int o, int um){
+  switch(o){
+  case 0:
+    return 700;
+   break; 
+  case 1:
+    return 200 + um;
+    break;
+  
+  case 2:
+     return p*2;
+     break;
+  }
+}
+
 String arrayMessage(){
   String mess = "{";
-  for(int i = 0; i<10;i++){
+  for(int i = 0; i<largoArray ;i++){      // funcion para mostrar los elementos del array
     mess += String(arraySensor[i])+ ", ";
-    } 
+  } 
     mess += "}";
     return mess;
   } 
 void setup() {
+   led = 8;
+  pinMode(led, OUTPUT);
   //myservo.attach(6);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
 }
 
 void loop() {
-   sensorValue = analogRead(A3);
-   //Serial.println(sensorValue);
+  regulaUmbral = analogRead(A2);
+  sensorValue = analogRead(A3);
   cont = promValues(sensorValue);
-  Serial.println(String(sensorValue) +","+ String(cont));
+  Serial.println(String(sensorValue) +","+ String(cont)+ ", "+ String(getUmbral(cont,optionUmbral,regulaUmbral)));
+  if(sensorValue > getUmbral(cont, optionUmbral,regulaUmbral)){
+    digitalWrite(led, HIGH);
+    }
+    else{
+      digitalWrite(led, LOW);
+      }
   delay(100);        // delay in between reads for stability
   servoValue = map(sensorValue, 100, 800, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
  // myservo.write(servoValue);                  // sets the servo position according to the scaled value
